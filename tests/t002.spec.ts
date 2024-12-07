7dfd8747a005c4037bb8404aba43bb902a1bbc75
Usan Sakhakarmi <beenage9@gmail.com>
2024-12-07 11:15:28 +0545

    pageObject

--- tests/t002.spec.ts
+++ tests/t002.spec.ts
@@ -1,48 +1,50 @@
 import {test,expect} from '@playwright/test'
-import exp from 'constants';
+import {locators} from '../pageObject/login/locator'
+
+let baseurl = `https://www.saucedemo.com/`;
+
+test.beforeEach(async({page})=>{
+    await page.goto(baseurl);
+})
+
 test.describe('validate sauce login functionality',()=>{
-    let baseurl = `https://www.saucedemo.com/`;
-    let username = `input[name="user-name"]`;
-    let password = `input[id="password"]`;
-    let login = `input[class="submit-button btn_action"]`;
+
     test('validate with valid username and valid password',async({page})=>{
-        await page.goto(baseurl);
-        await page.locator(username).fill('standard_user');
-        await page.locator(password).fill('secret_sauce');
-        await page.locator(login).click();
+        await page.locator(locators.username).fill('standard_user');
+        await page.locator(locators.password).fill('secret_sauce');
+        await page.locator(locators.login).click();
         await expect(page.locator(`div[class="app_logo"]`)).toBeVisible();
     });
     test('validate with valid username and invalid password',async({page})=>{
-        await page.goto(baseurl);
-        await page.locator(username).fill('standard_user');
-        await page.locator(password).fill('standard_user');
-        await page.locator(login).click();
+        await page.locator(locators.username).fill('standard_user');
+        await page.locator(locators.password).fill('standard_user');
+        await page.locator(locators.login).click();
         await expect(page.getByRole('heading',{name: ' password do not match'})).toBeVisible();
     });
     test('validate with invalid username and valid password',async({page})=>{
         await page.goto(baseurl);
-        await page.locator(username).fill('user');
-        await page.locator(password).fill('secret_sauce');
-        await page.locator(login).click();
+        await page.locator(locators.username).fill('user');
+        await page.locator(locators.password).fill('secret_sauce');
+        await page.locator(locators.login).click();
         await expect(page.getByRole('heading',{name: ' password do not match'})).toBeVisible();
     });
     test('validate with invalid username and invalid password',async({page})=>{
         await page.goto(baseurl);
-        await page.locator(username).fill('user');
-        await page.locator(password).fill('standard_user');
-        await page.locator(login).click();
+        await page.locator(locators.username).fill('user');
+        await page.locator(locators.password).fill('standard_user');
+        await page.locator(locators.login).click();
         await expect(page.getByRole('heading',{name: ' password do not match'})).toBeVisible();
     });
     test('validate with valid username and empty password',async({page})=>{
         await page.goto(baseurl);
-        await page.locator(username).fill('standard_user');
-        await page.locator(login).click();
+        await page.locator(locators.username).fill('standard_user');
+        await page.locator(locators.login).click();
         await expect(page.getByRole('heading',{name: ' password is required'})).toBeVisible();
     });
     test('validate with empty username and valid password',async({page})=>{
         await page.goto(baseurl);
-        await page.locator(password).fill('secret_sauce');
-        await page.locator(login).click();
+        await page.locator(locators.password).fill('secret_sauce');
+        await page.locator(locators.login).click();
         await expect(page.getByRole('heading',{name: 'username is required'})).toBeVisible();
     });
 })
@@ -53,9 +55,7 @@ test.describe('validating sauce login2',()=>{
     let password = `input[id="password"]`;
     let login = `input[class="submit-button btn_action"]`;
 
-    test.beforeEach(async({page})=>{
-        await page.goto(baseurl);
-    })
+    
     test('validate with valid username and valid password',async({page})=>{
         await page.locator(username).fill('standard_user');
         await page.locator(password).fill('secret_sauce');
--- tests/t003.spec.ts
+++ tests/t003.spec.ts
@@ -1,21 +1,24 @@
 import {expect,test} from "@playwright/test";
-
+import loginA1  from "../pageObject/login/login";
 test.describe('validating sauce login2',()=>{
-    let baseurl = `https://www.saucedemo.com/`;
+    // let baseurl = `https://www.saucedemo.com/`;
     
-    test.beforeEach(async({page})=>{
-        await page.goto(baseurl);
-    })
-    test.afterEach(async({page})=>{
-        await page.goto(baseurl);
-    })
-    test.beforeAll(async({page})=>{
-        await page.goto(baseurl);
-    })
-    test.afterAll(async({page})=>{
-        await page.goto(baseurl);
-    })
+    // test.beforeEach(async({page})=>{
+    //     await page.goto(baseurl);
+    // })
+    // test.afterEach(async({page})=>{
+    //     await page.goto(baseurl);
+    // })
+    // test.beforeAll(async({page})=>{
+    //     await page.goto(baseurl);
+    // })
+    // test.afterAll(async({page})=>{
+    //     await page.goto(baseurl);
+    // })
     test('validate with valid username and valid password',async({page})=>{
+        const login = new loginA1(page);
+        await login.gotoLoginPage();
+        await login.login("standard_user","secret_sauce");
     });
     test('validate with valid username and invalid password',async({page})=>{
     });
